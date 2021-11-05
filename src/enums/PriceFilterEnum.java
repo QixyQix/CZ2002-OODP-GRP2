fcd4f9e397e9ed9f0cf80e96efdc3ca2348d6888
marvenlee2486 <leezongyu6878@gmail.com>
2021-11-05 15:34:40 +0800

    Add PriceFilters

---
+++ src/enums/PriceFilterEnum.java
@@ -0,0 +1,20 @@
+package enums;
+
+
+public enum PriceFilterTypeEnum{
+    ABSOLUTE,
+    PERCENTAGE,
+
+}
+
+public enum TaxFilterNameEnum {
+    SERVICE_CHARGE,
+    GST,
+}
+
+public enum DiscountFilterNameEnum{
+    GOLD_MEMBERSHIP,
+    SILVER_MEMBERSHIP,
+    BRONZE_MEMBERSHIP,
+    NO_MEMBERSHIP
+}

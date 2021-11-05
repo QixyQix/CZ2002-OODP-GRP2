fe010550d8407d137ac551d469c0e705def24f02
marvenlee2486 <leezongyu6878@gmail.com>
2021-11-05 16:07:12 +0800

    Add Report, templates for Mgr

---
+++ src/entities/Report.java
@@ -0,0 +1,57 @@
+
+Class Report{
+
+    private ArrayList<Invoice> invoiceList;
+    private double totalRevenue;
+    private LocalDate date;
+
+    public Report(LocalDate date){
+        this.date = date;
+    }
+
+    private void calculateTotalRevenue(){
+        if(!validator()){
+            System.out.println("There are Invoice that not belong to the date, probably insert wrongly. Please Check");
+            return;
+        }
+        this.totalRevenue =0;
+        for (int i =0; i< this.invoiceList.size();i++){
+            this.totalRevenue += this.invoiceList.get(i).getFinalPrice();
+        }
+    }
+
+    private boolean validator(){
+
+        // TODO, to be able to fine out which invoicenumber placed wrongly ..
+        for(int i =0; i<this.invoiceList.size();i++){
+            if ( this.invoiceList.get(i).getDate().getOrder().getDate().toLocalDate()  != date)
+                return false;            
+        }
+        return true;
+    }
+
+    public ArrayList<Invoice> getInvoiceList(){
+        return this.ArrayList;
+    }
+
+    public void addInvoiceList(Invoice invoice){
+        this.invoiceList.add(invoice);
+    }
+
+    public void getTotalRevenue(){
+        calculateTotalRevenue();
+        return this.totalRevenue;
+    }
+
+    public LocalDate getDate(){
+        return this.date;
+    }
+
+    public boolean getByMonth(){
+        return this.byMonth;
+    }   
+    
+}
+
+    
+

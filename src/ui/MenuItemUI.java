0915bf7a24ca7bbc837484d41b3cf40147c204a0
Qi Xiang <qixyqix@outlook.com>
2021-11-06 22:20:06 +0800

    Add CreateMenuItem UI

--- src/ui/MenuItemUI.java
+++ src/ui/MenuItemUI.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import entities.MenuItem;
@@ -32,6 +33,7 @@ public final class MenuItemUI {
             displayOptions();
             try {
                 option = sc.nextInt();
+                sc.nextLine();
             } catch (Exception ex) {
                 System.out.println("Invalid input");
                 continue;
@@ -41,7 +43,9 @@ public final class MenuItemUI {
             case 1:
                 showCurrentMenuItems();
                 break;
-
+            case 2:
+                createMenuItem();
+                break;
             }
         } while (option != -1);
     }
@@ -59,7 +63,6 @@ public final class MenuItemUI {
         System.out.println("MENU ITEMS:");
         try {
             MenuItem[] items = MenuItemMgr.getInstance().getAllMenuItems();
-            System.out.println(items.length);
 
             for (MenuItem item : items) {
                 System.out.println("ID: " + item.getId() + " | Type: " + item.getType() + " | Name: " + item.getName()
@@ -69,6 +72,84 @@ public final class MenuItemUI {
         } catch (Exception ex) {
             System.out.println("An error occured while getting all Menu Items:");
             System.out.println(ex.getMessage());
+        }
+    }
+
+    private void createMenuItem() {
+        boolean invalid = false;
+        boolean createPackage = false;
+
+        int id = 0;
+        String type;
+        String name;
+        String description;
+        double price =0.0;
+        ArrayList<MenuItem> packageItems = null;
+
+        System.out.println("Is this a package? Enter P for package.");
+        if (sc.nextLine().equals("P")) {
+            createPackage = true;
+            packageItems = new ArrayList<MenuItem>();
+        }
+
+        do {
+            try {
+                System.out.println("Enter an ID for the Menu Item");
+                id = sc.nextInt();
+                sc.nextLine();
+                if (!MenuItemMgr.getInstance().checkIDAvailable(id)) {
+                    System.out.println("ID " + id + " has already been taken. Please enter another ID.");
+                    continue;
+                }
+            } catch (Exception ex) {
+                System.out.println("Enter a valid ID");
+            }
+        } while (invalid);
+
+        System.out.println("Enter the item type: ");
+        type = sc.nextLine();
+
+        System.out.println("Enter a name: ");
+        name = sc.nextLine();
+
+        System.out.println("Enter a description: ");
+        description = sc.nextLine();
+
+        do {
+            invalid = true;
+            System.out.println("Enter a price: ");
+            try {
+                price = sc.nextDouble();
+                sc.nextLine();
+
+                if (price < 0.01) {
+                    System.out.println("Please enter a price greater than 0.01");
+                    continue;
+                }
+                invalid = false;
+            } catch (Exception ex) {
+                System.out.println("Please enter a valid double");
+            }
+        } while (invalid);
+
+        if(createPackage){
+            System.out.println("Enter the IDs of items to be included in package (-1) to end: ");
+            int idToAdd = 0;
+            do{
+                try{
+                    idToAdd = sc.nextInt();
+                    sc.nextLine();
+                    packageItems.add(MenuItemMgr.getInstance().getMenuItemByID(idToAdd));
+                }catch(Exception ex){
+                    System.out.println("Please enter a valid ID");
+                }
+            }while(idToAdd > 0);
+        }
+
+        try{
+            MenuItemMgr.getInstance().createMenuItem(type, name, description, price, id, packageItems);
+        }catch(Exception ex){
+            System.out.println("An error occured while creating menu item");
         }
     }
 

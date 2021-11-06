423d13990bcab7771ad5b9a5ebec00a82768c558
Qi Xiang <qixyqix@outlook.com>
2021-11-06 16:06:33 +0800

    Update menuItemFactory

--- src/factories/MenuItemFactory.java
+++ src/factories/MenuItemFactory.java
@@ -2,6 +2,7 @@ package factories;
 
 import entities.MenuItem;
 import entities.MenuPackage;
+import enums.MenuItemState;
 
 public final class MenuItemFactory {
     private static MenuItemFactory INSTANCE;
@@ -17,13 +18,25 @@ public final class MenuItemFactory {
         return INSTANCE;
     }
 
-    public MenuItem createMenuItem(String itemType) throws ClassNotFoundException {
+    public MenuItem createMenuItem(String itemType, String type, String name, String description, double price, int stocks, MenuItemState state, int id, MenuItem[] packageItems) throws ClassNotFoundException {
         switch (itemType.toUpperCase()) {
         case "ALACARTE":
-            return new MenuItem();
+            MenuItem alaCarteItem = new MenuItem(type, name, description, price, stocks, state, id);
+            return alaCarteItem;
         case "PACKAGE":
-            return new MenuPackage();
+            MenuItem packageItem = new MenuPackage(type, name, description, price, stocks, state, id);
+            return packageItem;
         default:
+            throw new ClassNotFoundException();
+        }
+    }
+
+    public MenuItem createMenuItemFromSerializedData(Object o) throws ClassNotFoundException{
+        if(o instanceof MenuPackage){
+            return (MenuPackage) o;
+        }else if(o instanceof MenuItem){
+            return (MenuItem) o;
+        }else{
             throw new ClassNotFoundException();
         }
     }

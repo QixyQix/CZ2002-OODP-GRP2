f54ae725335101c20aa206d77bbb92743accbd2b
Benjamin Ho <benjaminhojh@gmail.com>
2021-11-06 17:15:09 +0800

    OrderMgr

---
+++ src/managers/OrderMgr.java
@@ -0,0 +1,22 @@
+package managers;
+
+import java.util.HashMap;
+
+import entities.MenuItem;
+
+public final class OrderMgr {
+    private static OrderMgr INSTANCE;
+    private HashMap<Integer, MenuItem> items;
+    
+    public static OrderMgr getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new OrderMgr();
+        }
+
+        return INSTANCE;
+    }
+    public void allocateTable(){}
+    public void chooseItem(){}
+    public void closeOrder(Order order){}
+    
+}

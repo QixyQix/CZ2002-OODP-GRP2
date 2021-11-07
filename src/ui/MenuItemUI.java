ae9420ecce9d85eaa01a8ba03e36b8a33f31cda1
Qi Xiang <qixyqix@outlook.com>
2021-11-07 23:00:30 +0800

    Added editMenuItem to MenuItemUI

--- src/ui/MenuItemUI.java
+++ src/ui/MenuItemUI.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import entities.MenuItem;
 import managers.MenuItemMgr;
 
-public final class MenuItemUI extends UserInterface{
+public final class MenuItemUI extends UserInterface {
     private static MenuItemUI INSTANCE;
     private Scanner sc;
 
@@ -47,6 +47,9 @@ public final class MenuItemUI extends UserInterface{
             case 2:
                 createMenuItem();
                 break;
+            case 3:
+                editMenuItem();
+                break;
             case 4:
                 deleteMenuItem();
                 break;
@@ -81,27 +84,26 @@ public final class MenuItemUI extends UserInterface{
     }
 
     private void createMenuItem() {
-        boolean invalid = false;
         boolean createPackage = false;
 
         int id = 0;
         String type;
         String name;
         String description;
-        double price =0.0;
+        double price = 0.0;
         ArrayList<MenuItem> packageItems = null;
 
-        if(super.getInputString("Is this a package? Enter P for package.").equals("P")){
+        if (super.getInputString("Is this a package? Enter P for package.").equals("P")) {
             createPackage = true;
             packageItems = new ArrayList<MenuItem>();
         }
 
-        while(true){
-            id = super.getInputInt("Enter an ID for the Menu Item",0, Integer.MAX_VALUE);
-            if(!MenuItemMgr.getInstance().checkIDAvailable(id)){
+        while (true) {
+            id = super.getInputInt("Enter an ID for the Menu Item", 0, Integer.MAX_VALUE);
+            if (!MenuItemMgr.getInstance().checkIDAvailable(id)) {
                 System.out.println("ID " + id + " has already been taken. Please enter another ID.");
                 continue;
-            }else{
+            } else {
                 break;
             }
         }
@@ -111,41 +113,95 @@ public final class MenuItemUI extends UserInterface{
         description = super.getInputString("Enter the item description: ");
         price = super.getInputDouble("Enter the item price: ", 0.0, Double.MAX_VALUE);
 
-
-        if(createPackage){
+        if (createPackage) {
             System.out.println("Enter the IDs of items to be included in package (-1) to end: ");
             int idToAdd = 0;
-            do{
+            do {
                 idToAdd = super.getInputInt("Enter ID: ", -1, Integer.MAX_VALUE);
-                try{
+                try {
                     MenuItem itemToAdd = MenuItemMgr.getInstance().getMenuItemByID(idToAdd);
                     packageItems.add(itemToAdd);
-                    System.out.println("Added item "+itemToAdd.getName() + " to package");
-                }catch(Exception ex){
+                    System.out.println("Added item " + itemToAdd.getName() + " to package");
+                } catch (Exception ex) {
                     System.out.println("Please enter a valid ID");
                 }
-            }while(idToAdd > 0);
+            } while (idToAdd > 0);
         }
 
-        try{
+        try {
             MenuItemMgr.getInstance().createMenuItem(type, name, description, price, id, packageItems);
             System.out.println("Menu Item Created");
-        }catch(Exception ex){
+        } catch (Exception ex) {
             System.out.println("An error occured while creating menu item");
         }
     }
 
-    private void deleteMenuItem(){
-        while(true){
-            int idToRemove = super.getInputInt("Enter ID of item to delete: ",0,Integer.MAX_VALUE);
-            try{
+    private void deleteMenuItem() {
+        while (true) {
+            int idToRemove = super.getInputInt("Enter ID of item to delete: ", 0, Integer.MAX_VALUE);
+            try {
                 MenuItemMgr.getInstance().deleteMenuItemByID(idToRemove);
                 System.out.println("Item removed");
                 break;
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 System.out.println("Invalid ID");
             }
         }
+    }
+
+    private void editMenuItem() {
+        MenuItem itemToEdit = null;
+        boolean loop = true;
+
+        do {
+            int id = super.getInputInt("Enter ID of item to edit: ");
+            try {
+                itemToEdit = MenuItemMgr.getInstance().getMenuItemByID(id);
+                loop = false;
+            } catch (Exception ex) {
+                System.out.println("Invalid item ID");
+            }
+        } while (loop);
+
+        loop = true;
+        do {
+            // TODO Manage package items
+            System.out.println("== EDIT ITEM ID: " + itemToEdit.getId() + " ==");
+            System.out.println("(1) type: " + itemToEdit.getType());
+            System.out.println("(2) name: " + itemToEdit.getName());
+            System.out.println("(3) description: " + itemToEdit.getDescription());
+            System.out.println("(4) price: " + itemToEdit.getPrice());
+            System.out.println("(-1) Exit");
+            int choice = super.getInputInt("Enter number of corresponding property to edit", -1, 4);
+            switch (choice) {
+            case 1:
+                String newType = super.getInputString("Enter new value for type:");
+                itemToEdit.setType(newType);
+                System.out.println("New type saved.");
+                break;
+            case 2:
+                String newName = super.getInputString("Enter new value for name:");
+                itemToEdit.setName(newName);
+                System.out.println("New name saved.");
+                break;
+            case 3:
+                String newDesc = super.getInputString("Enter new value for description:");
+                itemToEdit.setDescription(newDesc);
+                System.out.println("New description saved.");
+                break;
+            case 4:
+                double newPrice = super.getInputDouble("Enter new valuue for price: ", 0.1, Double.MAX_VALUE);
+                itemToEdit.setPrice(newPrice);
+                System.out.println("New price saved.");
+                break;
+            case -1:
+                loop = false;
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+            }
+        } while (loop);
     }
 
 }

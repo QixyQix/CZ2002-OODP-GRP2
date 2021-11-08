39d060474914a04bcfd1f350f05df5a828723904
Benjamin Ho <benjaminhojh@gmail.com>
2021-11-08 20:12:36 +0800

    updated tableui

--- src/ui/TableUI.java
+++ src/ui/TableUI.java
@@ -2,8 +2,13 @@ package ui;
 
 import managers.TableMgr;
 
+import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+
 public class TableUI {
     private static TableUI instance;
+    private static Scanner sc = new Scanner(System.in);
 
     private TableUI() {
     };
@@ -23,10 +28,57 @@ public class TableUI {
     public void displayOptions() {
         System.out.println("====Table Manager====");
         System.out.println("(1) Check table availability");
+        System.out.println("(-1) Exit");
     }
 
-    public boolean checkTableAvailability() {
+    public void selectOption() {
+        int option = 0;
+        int noOfPax;
+        LocalDateTime date;
+        do {
+            displayOptions();
+            try {
+                option = sc.nextInt();
+                sc.nextLine();
+            } catch (Exception ex) {
+                System.out.println("Invalid input");
+                continue;
+            }
+
+            switch (option) {
+            case 1:
+                while (true) {
+                    try {
+                        System.out.println("How many pax?: ");
+                        noOfPax = sc.nextInt();
+                        sc.nextLine();
+                        break;
+                    } catch (Exception ex) {
+                        System.out.println("Invalid input");
+                        continue;
+                    }
+                }
+                while (true) {
+                    try {
+                        System.out.println("Which date and time? (yyyy-MM-dd HH:mm): ");
+                        String dateString = sc.nextLine().trim().replace(" ", "T");
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+                        date = LocalDateTime.parse(dateString, formatter);
+                        sc.nextLine();
+                        break;
+                    } catch (Exception ex) {
+                        System.out.println("Invalid input");
+                        continue;
+                    }
+                }
+                checkTableAvailability(noOfPax, date);
+                break;
+            }
+        } while (option != -1);
+    }
+
+    public boolean checkTableAvailability(int noOfPax, LocalDateTime date) {
         TableMgr tablemgr = TableMgr.getInstance();
-        return tablemgr.checkTableAvailability;
+        return tablemgr.checkTableAvailability(noOfPax, date);
     }
 }

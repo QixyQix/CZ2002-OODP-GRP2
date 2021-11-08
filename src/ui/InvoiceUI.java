df00d33c1051a7ff4c24b64c3575b51b47c98289
yankai14 <limyk2014@gmail.com>
2021-11-08 20:18:22 +0800

    Added InvoiceUI

---
+++ src/ui/InvoiceUI.java
@@ -0,0 +1,40 @@
+package ui;
+
+import java.util.Scanner;
+import managers.InvoiceMgr;
+
+
+public final class InvoiceUI extends UserInterface {
+    private static InvoiceUI INSTANCE;
+    private Scanner sc;
+
+    private InvoiceUI() {
+        super();
+        this.sc = new Scanner(System.in);
+    }
+
+    public static InvoiceUI getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new InvoiceUI();
+        }
+
+        return INSTANCE;
+    }
+
+    private void displayOptions() {
+        System.out.println("====Payment Manager====");
+        System.out.println("(1) Close order and create invoice");
+        System.out.println("(2) Print order invoice");
+    }
+
+
+    private void createInvoice() {
+        
+        int orderid;
+
+        while(true) {
+            orderid = super.getInputInt("Enter your order id");    
+        }
+    }
+    
+}

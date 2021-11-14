0c4804c6a56c5a4c1661d05647735b8fd08833ae
Benjamin Ho <benjaminhojh@gmail.com>
2021-11-14 16:15:47 +0800

    javadocs added

--- src/package-search-index.js
+++ src/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"entities"},{"l":"managers"},{"l":"ui"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"entities"},{"l":"enums"},{"l":"exceptions"},{"l":"factories"},{"l":"global"},{"l":"managers"},{"l":"ui"}];updateSearchResults();
